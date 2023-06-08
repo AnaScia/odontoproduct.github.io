01a41dad85b54de569d4b29e824fc7fd4476f6ae
Anabella Sciangula <anabellasciangula@gmail.com>
2023-06-08 11:14:00 -0300

    add new file

---
+++ login.js
@@ -0,0 +1,31 @@
+//LOGIN
+
+let btnLogin = document.getElementById("btnLogin");
+let emailLog = document.getElementById("email_log");
+let passLog = document.getElementById("pass_log");
+
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  const data1 = {
+    email: emailLog.value,
+    password: passLog.value,
+  };
+  console.log(data1);
+});
+
+//REGISTER
+
+let btnReg = document.getElementById("btnReg");
+let userReg = document.getElementById("user_reg");
+let emailReg = document.getElementById("email_reg");
+let passReg = document.getElementById("pass_reg");
+
+btnReg.addEventListener("click", (e) => {
+  e.preventDefault();
+  const data2 = {
+    username: userReg.value,
+    email: emailReg.value,
+    password: passReg.value,
+  };
+  console.log(data2);
+});
